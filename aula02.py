67843d0fd01fb72e49047f310d00feb735cf7cd2
Melissa Kakuno <kalisekakuno@gmail.com>
2026-02-18 12:44:02 -0300

    Primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 480 # variável int
+numero_quebrado = 1.25 # variável float
+a_caneta_esta_em_cima_da_mesa = True #variável boolean
+texto = "Primeira aula pratica" # variável string 
+
+
+print(numero)
+print(numero_quebrado)
+print(a_caneta_esta_em_cima_da_mesa)
+print(texto)
