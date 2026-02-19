a04a2e2fcc62a49194dcd082112fba61db1bcbc0
Melissa Kakuno <kalisekakuno@gmail.com>
2026-02-19 09:50:39 -0300

    aula para praticar

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "coelho" #variável string
+idade = 3 # variável int
+peso = 1.5 # variável float
+sexo = "femea" # variável string 
+
+
+print (nome)
+print (idade)
+print (peso)
+print (sexo)
