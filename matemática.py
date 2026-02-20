a845c0a86691f831a0433fc461a76222819825d6
Melissa Kakuno <kalisekakuno@gmail.com>
2026-02-20 09:50:22 -0300

    Aula prática matemática/aluno

---
+++ matemática.py
@@ -0,0 +1,15 @@
+valor1 = int(input("digite o valor1"))
+
+valor2 = int(input("digite o valor"))
+
+
+
+soma = valor1 + valor2 
+subtracao = valor1 - valor2 
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print ("O resultado da soma é" , soma)
+print ("O resultado da subtracao é" , subtracao)
+print ("O resultado da multiplicacao é" , multiplicacao)
+print ("O resultado da divisao é" , divisao)
