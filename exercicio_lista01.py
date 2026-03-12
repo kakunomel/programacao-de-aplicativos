ce5f93e1ce719907cf6c9d5a2ca82fbe31435fe8
Melissa Kakuno <kalisekakuno@gmail.com>
2026-03-12 10:03:56 -0300

    Aula dia 12/03

---
+++ exercicio_lista01.py
@@ -0,0 +1,4 @@
+filme = ["Simplesmente Acontece" , "A cinco passos de você" , "A culpa é das estrelas" , "Orgulho e Preconceito" , "Como eu era antes de você"]
+
+print(f"1º filme: {filme[0]}")
+print(f"5º filme: {filme[4]}")
---
+++ exercicio_lista02.py
@@ -0,0 +1,5 @@
+estoque = ["Teclado" , "Mouse" , "Monitor"] 
+print(f"Estoque antigo: {estoque}")
+
+estoque = ["Teclado" , "Mouse" , "Webcam"]
+print(f"Estoque atualizado: {estoque}")
