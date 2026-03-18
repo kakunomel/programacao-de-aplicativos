a5a592fef516f7b3b6fc2c3fb1094c1c073f69ab
Melissa Kakuno <kalisekakuno@gmail.com>
2026-03-18 12:42:56 -0300

    Aula dia 18/03

---
+++ exercicio_append_insert.py
@@ -0,0 +1,8 @@
+fila = ["Ana" , "Bruno" , "Caio"]
+print("A lista está com os seguintes nomes" , fila)
+
+fila.append("Daniel")
+print("Lista atualizada" , fila)
+
+fila.insert(0,"Zuleica")
+print("Lista atualizada novamente" , fila)
